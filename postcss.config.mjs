d7c144d3fb0919842c312c4a1717d68be355fd41
mawell <82365695+man4460@users.noreply.github.com>
2025-04-19 11:14:05 +0700

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,3 +3,9 @@ const config = {
 };
 
 export default config;
+
+module.exports = {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
+    },
+  };
